41e729ac7deba710af629f04ffed422607278c4e
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-02-16 10:15:42 +0200

    Add Result class and NetworkError to handle API calls.

---
+++ app/src/main/java/com/example/myapplication/core/domain/NetworkError.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.core.domain
+
+enum class NetworkError: Error {
+    REQUEST_TIMEOUT,
+    TOO_MANY_REQUESTS,
+    NO_INTERNET,
+    SERVER_ERROR,
+    SERIALIZATION,
+    UNKNOWN,
+}
