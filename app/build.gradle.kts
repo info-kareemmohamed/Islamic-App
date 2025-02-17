969f2b4f82a6920c811ad8d3ce8f6178656d65e5
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-02-17 20:11:48 +0200

    Handled dependencies in Gradle for bundling, added YouTube player integration to play videos, and included testing libraries such as MockK, Turbine, and kotlinx-coroutines-test for unit and integration testing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,25 +42,23 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.bundles.compose)
+
+    testImplementation(libs.bundles.testing)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.bundles.androidTest)
+
+    debugImplementation(libs.bundles.debug)
 
     implementation(libs.bundles.koin)
     implementation(libs.bundles.ktor)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.coil.compose)
 
+    implementation(libs.youtube.player)
 }
